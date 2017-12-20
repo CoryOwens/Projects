27018411b107c3db112c8d585877d4d430a6326e
cowens <cowens@cradlepoint.com>
2017-12-20 13:41:08 -0700

    Added solution for Sieve of Eratosthenes.

---
+++ languages/python3/02_classic_algorithms/4_sieve_of_eratosthenes/sieve.py
@@ -0,0 +1,14 @@
+import math
+
+def sieve(n):
+    """Return all primes p such that p <= n"""
+    if n < 2:
+        raise ValueError('Provide n >= 2. n:{}'.format(n))
+    a = [False, False] + [True] * (n-1)
+    for i in range(2, int(math.sqrt(n))+1):
+        if a[i]:
+            j = i**2
+            while j <= n:
+                a[j] = False;
+                j += i
+    return [i for i in range(n+1) if a[i]]
